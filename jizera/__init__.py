5d3d58a748c1db19345945000b52e8c9865d3ccb
gronki <gronki@gmail.com>
2016-09-22 18:52:43 +0200

    komentarze

--- jizera/__init__.py
+++ jizera/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# tworzymy instancje flaska
 app = Flask('Jizera')
+
+# pare ustawien
 testdb_location = '/tmp/jizera-testing.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + testdb_location
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# tworzymy obiekt SQLAlchemy
 db = SQLAlchemy(app)
 
 from models import *
