d51b4f306ba557a0f7ddba89dddab319def645bf
gronki <gronki@gmail.com>
2016-11-06 19:02:57 +0100

    Działa wyświetlanie szablonu.

--- jizera/__init__.py
+++ jizera/__init__.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # tworzymy instancje flaska
 app = Flask('Jizera')
 
 from jizera.database import *
+from jizera.models import *
+
+@app.route('/')
+def index():
+    init_db()
+    data = dbsession.query(Observation.date_start,Observer.name,Observer.lastname,
+        Location.name, Location.latitude, Location.longitude, MeteorData.magnitude,
+        DSLRData.magnitude, SQMData.magnitude_z) \
+        .join(Observer,Location,MeteorData,DSLRData,SQMData).all()
+    destroy_db()
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run()
--- jizera/database.py
+++ jizera/database.py
@@ -2,18 +2,19 @@
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from jizera import app
 
 db_location = '/tmp/jizera-testing.db'
 
-engine = create_engine('sqlite:///' + db_location, convert_unicode=True, echo=True)
+engine = create_engine('sqlite:///' + db_location, convert_unicode=True, echo=app.debug)
 dbsession = scoped_session(sessionmaker(bind=engine,autocommit=False))
 
 Base = declarative_base()
 Base.query = dbsession.query_property()
 
-# def init_db():
-from jizera.models import *
-Base.metadata.create_all(bind=engine)
+def init_db():
+    import jizera.models
+    Base.metadata.create_all(bind=engine)
 
 def destroy_db():
     dbsession.remove()
