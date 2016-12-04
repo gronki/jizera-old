13c1c59abafb12cff8ba703c265bd3e6fcdcdd35
gronki <gronki@gmail.com>
2016-12-04 10:19:45 +0100

    Wywalone filtry datetime -- są konwertery

--- jizera/filters.py
+++ jizera/filters.py
@@ -2,14 +2,6 @@
 from jizera import app
 from datetime import datetime
 
-@app.template_filter('sql_datetime')
-def parse_sql_datetime(s):
-    return datetime.strptime(s, r'%Y-%m-%d %H:%M:%S')
-
-@app.template_filter('sql_timestamp')
-def parse_sql_timestamp(s):
-    return datetime.strptime(s, r'%Y-%m-%d %H:%M:%S.%f')
-
 @app.template_filter('timeago')
 def filter_timeago(d):
     td = datetime.now() - d
@@ -22,12 +14,6 @@ def filter_timeago(d):
     else:
         return u"%d dni temu" % td.days
 
-
 @app.template_filter('onlydate')
 def filter_onlydate(d):
     return d.strftime('%y-%m-%d')
-
-@app.template_filter('latlng')
-def filter_latlong(latlng):
-    lat,lng = latlng
-    return u"@%0.3f,%0.3f" % (lat,lng)
