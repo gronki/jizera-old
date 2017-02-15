007cafc1d4b26773b9e219072d70d4e6923a520c
gronki <gronki@gmail.com>
2017-02-15 23:11:37 +0100

    Przeniesienie aby dało się spakować

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name = 'jizera',
+setup(
+    name = 'jizera',
     version = '170214',
     license = 'MIT',
     author = 'Dominik Gronkiewicz',
     author_email = 'gronki@gmail.com',
     url = 'https://github.com/gronki/jizera',
-    packages = ['jizera'],
+    packages = find_packages(),
     install_requires = [
         'flask',
         'loremipsum',
     ],
-    include_package_data=True,
+    include_package_data = True,
+    zip_safe = False,
 )
