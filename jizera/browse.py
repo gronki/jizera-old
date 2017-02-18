de880a2197f2b9a09a47bd9d02a733f284a4816d
gronki <gronki@gmail.com>
2017-02-18 08:17:29 +0100

    puste pliki na browser

--- jizera/browse.py
+++ jizera/browse.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from flask import render_template
 from jizera import app
 from jizera.db import get_db
 
 @app.route('/browse')
 def browser():
-    return "This is about to be implemented."
+    db = get_db()
+    return render_template("browse.html")
