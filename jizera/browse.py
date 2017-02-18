b6b3325821ac315cd69c452f6fead677bcc705af
gronki <gronki@gmail.com>
2017-02-18 08:57:33 +0100

    wyswietlanie brzydkiej tabelki z obserwacjami

--- jizera/browse.py
+++ jizera/browse.py
@@ -7,4 +7,24 @@ from jizera.db import get_db
 @app.route('/browse')
 def browser():
     db = get_db()
-    return render_template("browse.html")
+    cur = db.cursor()
+    cur.execute("""
+        SELECT
+        locations.name as location_name,
+        locations.latitude, locations.longitude,
+        (
+        	select count(*)
+        	from observations
+        	where observations.location_id = locations.id
+        ) as observations_count,
+        (
+        	select observations.created
+        	from observations
+        	where observations.location_id = locations.id
+        	order by observations.created desc limit 1
+        ) as last_observation_created
+        from locations
+        order by last_observation_created desc limit 10;
+        """)
+    data = cur.fetchall()
+    return render_template("browse.html", obslist=data)
